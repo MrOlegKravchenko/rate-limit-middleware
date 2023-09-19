403319c2a4dd4d326fe18f1aa1b463664a60e3bd
Oleg Kravchenko <mr.oleg.kravchenko@mail.ru>
2023-09-20 01:22:46 +0200

    Add a bench of interfaces and a User constructor with build in methods like limitCheck, pushNewRequest, onHold.

--- src/rateLimitMiddleware/rateLimitMiddleware.ts
+++ src/rateLimitMiddleware/rateLimitMiddleware.ts
@@ -1,18 +1,66 @@
-const users = {
-    ids: [],
-    info: {}
+interface IRequest {
+    requestId: string,
+    receiveTime: number
+}
+interface IUser {
+    userId: string,
+    requestsStack: IRequest[],
+    pushNewRequest: (r: IRequest) => void,
+    limitCheck: () => boolean,
+    onHold?: () => boolean
+}
+interface IUsers {
+    userIds: string[],
+    userInfos: {
+        [user: string]: IUser
+    }
+}
+
+class User {
+    userId: string;
+    requestsStack: IRequest[];
+    maxRequests: number;
+    interval: number;
+    constructor(userId: string, requestsStack: IRequest[], maxRequests: number, interval: number) {
+        this.userId = userId;
+        this.requestsStack = requestsStack;
+        this.maxRequests = maxRequests;
+        this.interval = interval;
+    }
+
+    limitCheck(): boolean {
+        if (this.requestsStack.length >= this.maxRequests) {
+            const lastRequestTimeByNFromEnd = (n: number): number => this.requestsStack[this.requestsStack.length - 1 - n].receiveTime;
+            const delta: number = lastRequestTimeByNFromEnd(0) - lastRequestTimeByNFromEnd(this.maxRequests);
+
+            return delta < this.interval;
+        }
+        return false;
+    }
+    pushNewRequest(userRequest: IRequest): void {
+        this.requestsStack.push(userRequest);
+    }
+    onHold(): boolean {
+        return this.limitCheck();
+    }
+}
+
+const users: IUsers = {
+    userIds: [],
+    userInfos: {}
 };
 
 const rateLimitMiddleware = ({ userId, maxRequests, interval }): void => {
-    console.log('userId', userId, users);
+    console.log('rateLimitMiddleware', userId, users, maxRequests, interval);
 
-    if( users.ids.includes(userId) ) {
-        users.info[userId].count++;
-        console.log('if', userId, users.info[userId]);
+    if( users.userIds.includes(userId) ) {
+        users.userInfos[userId].pushNewRequest(
+            { requestId: String(new Date()), receiveTime: Number(new Date()) });
+        console.log('if', userId, users.userInfos[userId]);
     } else {
-        users.ids.push(userId);
-        users.info[userId] = { count: 0 };
-        console.log('else', userId, users.info[userId]);
+        users.userIds.push(userId);
+        users.userInfos[userId] = new User(userId, [], maxRequests, interval);
+        console.log('else', userId, users.userInfos[userId]);
     }
 
     // if(limit exceeded) {
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.use(express.json())
 app.use((req, res, next) => {
-    const userId = req.headers['user-id'];
+    const userId: string | string[] = req.headers['user-id'];
 
     rateLimitMiddleware({ userId, maxRequests: 100, interval: 60 });
     next();
@@ -17,3 +17,13 @@ app.get('/api/user', (req, res) => {
 app.listen(port, () => {
     return console.log(`'Server is running on port ${port}`);
 });
+
+// TODO:
+// 0) Use User Constructor to build in methods to validate each User's data from inside;
+//
+// 1) Way to track 60 (interval) seconds:
+// - wait until 100 (maxRequests) request;
+// - check the diff between last request's receive time and request's receive time 100 before;
+//
+// 2) When limit triggered way to hold the requests to come:
+// Need onHold/isActive field which waits 60s before turn off.
